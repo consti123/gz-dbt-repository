0379b4f9a05d5b41e03591b3841a502afc587a9f
Constantin Oberbracht <Constantin.Oberbracht@gmail.com>
2025-10-30 11:29:55 +0000

    added some stuff

---
+++ models/int_orders_operational.sql
@@ -0,0 +1,11 @@
+SELECT
+a.orders_id
+,a.date_date 
+,a.revenue 
+,a.quantity
+,a.purchase_cost
+,a.margin
+, margin + shipping_fee - logcost - ship_cost AS operational_margin
+FROM {{ ref("int_orders_margin")}} AS a 
+INNER JOIN {{ ref("stg_raw__ship")}} AS b
+USING(orders_id)
--- models/int_sales_margin.sql
+++ models/int_sales_margin.sql
@@ -1,9 +1,7 @@
-
 SELECT
-revenue - (quantity*purchase_price) AS margin
+orders_id
+,revenue - (quantity*purchase_price) AS margin
 , quantity*purchase_price AS purchase_cost
-, quantity
-, revenue 
 FROM {{ ref("stg_raw__sales")}} AS a
 INNER JOIN {{ ref("stg_raw__product")}} AS b
 USING(products_id)
