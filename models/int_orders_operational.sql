5f520ba8085f29c54fa5af1c74f5b1366cdf7733
Constantin Oberbracht <Constantin.Oberbracht@gmail.com>
2025-10-30 12:20:51 +0000

    I did some stuff

--- models/int_orders_operational.sql
+++ models/int_orders_operational.sql
@@ -5,6 +5,9 @@ a.orders_id
 ,a.quantity
 ,a.purchase_cost
 ,a.margin
+, b.shipping_fee
+, b.logcost
+, b.ship_cost
 , margin + shipping_fee - logcost - ship_cost AS operational_margin
 FROM {{ ref("int_orders_margin")}} AS a 
 INNER JOIN {{ ref("stg_raw__ship")}} AS b
